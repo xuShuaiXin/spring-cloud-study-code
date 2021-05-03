ed88871700fc2f7ac4fe6d64a79950f335ddc278
xuShuaiXin <xu196404852@gmail.com>
2021-05-03 20:53:59 +0800

    为3355添加了SpringCloudConfig动态刷新功能

--- cloud-config-client-3355/src/main/java/com/xsx/springclou/controller/ConfigClientController.java
+++ cloud-config-client-3355/src/main/java/com/xsx/springclou/controller/ConfigClientController.java
@@ -1,17 +1,25 @@
 package com.xsx.springclou.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ConfigClientController {
+    @Value("${server.port}")
+    private String port;
+
     @Value("${config.info}")
     private String configInfo;
+
+    @Value("${config.version}")
+    private String version;
 
     @GetMapping("/configInfo")
     public String getConfigInfo()
     {
-        return configInfo;
+        return "port == " + port+ "\nconfigInfo ==" + configInfo + "\nversion == " + version;
     }
 }
