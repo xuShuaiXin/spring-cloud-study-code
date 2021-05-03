075bf616d4541e4f093da006e2fd80884da6867f
xuShuaiXin <xu196404852@gmail.com>
2021-05-03 11:53:54 +0800

    version 1.0.1

--- cloud-zuul-gateway9527/src/test/java/Test2.java
+++ cloud-zuul-gateway9527/src/test/java/Test2.java
@@ -1,5 +1,7 @@
 public class Test2 {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
     }
 }
