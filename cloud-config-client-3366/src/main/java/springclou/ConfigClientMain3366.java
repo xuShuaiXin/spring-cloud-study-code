365386a5956c5d15761244b10fb9b1b6ba7fcf9e
xuShuaiXin <xu196404852@gmail.com>
2021-05-05 10:05:51 +0800

    Config+bus+RabbitMQ 实现了广播通知的功能

--- cloud-config-client-3366/src/main/java/springclou/ConfigClientMain3366.java
+++ cloud-config-client-3366/src/main/java/springclou/ConfigClientMain3366.java
@@ -1,16 +1,14 @@
-package com.xsx.springclou;
+package springclou;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-import java.util.ArrayList;
-
 @SpringBootApplication
 @EnableEurekaClient
-public class ConfigClientMain3355 {
+public class ConfigClientMain3366 {
     public static void main(String[] args) {
-        SpringApplication.run(ConfigClientMain3355.class, args);
+        SpringApplication.run(ConfigClientMain3366.class, args);
 
     }
 }
---
+++ cloud-config-client-3366/src/main/java/springclou/controller/ConfigClientController.java
@@ -0,0 +1,25 @@
+package springclou.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class ConfigClientController {
+    @Value("${server.port}")
+    private String port;
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @Value("${config.version}")
+    private String version;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo()
+    {
+        return "port == " + port+ "\nconfigInfo ==" + configInfo + "\nversion == " + version;
+    }
+}
